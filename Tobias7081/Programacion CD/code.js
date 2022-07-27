acd5780defd0176d7bfc482b34299eaeb07b6fe6
Tobias-7081 <109706548+Tobias-7081@users.noreply.github.com>
2022-07-27 14:59:47 -0300

    Add files via upload

---
+++ Tobias7081/Programacion CD/code.js
@@ -0,0 +1,20 @@
+var nombre;
+var saludo;
+var frase1;
+var contenido1;
+/* nombre = prompt("Cual es tu nombre?"); */
+contenido1 = " bienvenido a el mejor lugar para aprender de tortugas";
+saludo = "Hola ";
+frase1 = saludo + nombre + contenido1;
+/* var frase1 = document.getElementById("frase1");
+frase1.style.color = "red"; */
+/* $(frase1).css("color", "red"); */
+/* var frase1 = document.getElementById('frase1');
+frase1.className = 'red'; */
+/* document.getElementById('estilos').href = 'style.css'; */
+document.write("<p id='parrafo'><script>document.write(frase1)</script></p>");
+const p = document.querySelector("#parrafo")
+p.style.color = 'green';
+p.style.position = 'relative';
+p.style.top = '40px';
+p.style.left = '350px';
